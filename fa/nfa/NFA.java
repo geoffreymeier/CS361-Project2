0f57291ebc2dfcad148f3053f8f0f02056222e35
Geoffrey Meier <geoffreymeier@u.boisestate.edu>
2020-04-01 14:49:32 -0600

    finished getDFA(), added .gitignore and README (todo)

--- fa/nfa/NFA.java
+++ fa/nfa/NFA.java
@@ -4,8 +4,8 @@ import java.util.LinkedHashSet;
 import java.util.Set;
 import java.util.HashSet;
 import java.util.Stack;
-import java.util.ArrayList;
-import java.lang.Math;
+// import java.util.ArrayList;
+// import java.lang.Math;
 
 import fa.dfa.DFA;
 
@@ -126,6 +126,22 @@ public class NFA implements NFAInterface {
 		return from.getTo(onSymb);
 	}
 
+	/**
+	 * Gets the set of "to" states based on a set of "from" states
+	 * 
+	 * @param from
+	 * @param onSymb
+	 * @return
+	 */
+	private Set<NFAState> getToState(Set<NFAState> from, char onSymb) {
+		Set<NFAState> ret = new HashSet<>();
+		for (NFAState s : from) {
+			ret.addAll(getToState(s, onSymb));
+		}
+
+		return ret;
+	}
+
 	@Override
 	public Set<Character> getABC() {
 		return ordAbc;
@@ -134,38 +150,76 @@ public class NFA implements NFAInterface {
 	@Override
 	public DFA getDFA() {
 		DFA dfa = new DFA();
-		dfa.addStartState("foo");
-		dfa.addFinalState("foo");
-		// dfa.addState(name);
-		Set<Set<NFAState>> ps = powerSet();
+		//create a stack to keep track of states we still need to visit
+		Stack<Set<NFAState>> stack = new Stack<Set<NFAState>>();
+		//create a set of sets of states to keep track of which states have already been added
+		Set<Set<NFAState>> states = new HashSet<Set<NFAState>>();
 
-		for (Set<NFAState> set : ps) {
-			System.out.println(set);
-			System.out.println(eClosure(set));
-			System.out.println("----");
+		Set<NFAState> start = eClosure(this.start);
+		dfa.addStartState(start.toString());
+		if (isFinal(start)) dfa.addFinalState(start.toString()); //check if start state is final
+		states.add(start);
+		stack.push(start);
+
+		// create set for alphabet, removing e if necessary
+		Set<Character> abc = ordAbc;
+		abc.remove('e');
+
+		while (!stack.isEmpty()) {
+			Set<NFAState> from = stack.pop();
+			for (char sym : abc) {
+				Set<NFAState> to = new HashSet<NFAState>();
+				to = eClosure(getToState(from, sym));
+
+				if (!states.contains(to)) {
+					if (isFinal(to))
+						dfa.addFinalState(to.toString());
+					else
+						dfa.addState(to.toString());
+
+					states.add(to);
+					stack.push(to);
+				}
+
+				dfa.addTransition(from.toString(), sym, to.toString());
+			}
 		}
 
 		return dfa;
 	}
 
-	private Set<Set<NFAState>> powerSet() {
-		Set<Set<NFAState>> ret = new HashSet<Set<NFAState>>();
-		
-		for (int i = 0; i < Math.pow(2, states.size()); i++)
-		{
-			Set<NFAState> s = new HashSet<NFAState>();
-			for (int j = 0; j < states.size(); j++) {
-				if (((i >> j) & 1) == 1) 
-				{
-					s.add(new ArrayList<>(states).get(j));
-				}
-			}
+	/**
+	 * Determines if, for a set of NFA states, any of them are final.
+	 * 
+	 * @param states set of states
+	 * @return true if at least one is final, false otherwise
+	 */
+	private boolean isFinal(Set<NFAState> states) {
+		boolean ret = false;
 
-			ret.add(s);
+		for (NFAState s : states) {
+			ret = ret || s.isFinal();
 		}
 
 		return ret;
 	}
+
+	// private Set<Set<NFAState>> powerSet() {
+	// 	Set<Set<NFAState>> ret = new HashSet<Set<NFAState>>();
+
+	// 	for (int i = 0; i < Math.pow(2, states.size()); i++) {
+	// 		Set<NFAState> s = new HashSet<NFAState>();
+	// 		for (int j = 0; j < states.size(); j++) {
+	// 			if (((i >> j) & 1) == 1) {
+	// 				s.add(new ArrayList<>(states).get(j));
+	// 			}
+	// 		}
+
+	// 		ret.add(s);
+	// 	}
+
+	// 	return ret;
+	// }
 
 	private Set<NFAState> eClosure(Set<NFAState> s) {
 		Set<NFAState> ret = new HashSet<NFAState>();
@@ -179,15 +233,15 @@ public class NFA implements NFAInterface {
 
 	@Override
 	public Set<NFAState> eClosure(NFAState s) {
-		//return values
+		// return values
 		LinkedHashSet<NFAState> ret = new LinkedHashSet<NFAState>();
 		// DFS stack
 		Stack<NFAState> stack = new Stack<NFAState>();
 
-		ret.add(s);	//add state itself to return set
+		ret.add(s); // add state itself to return set
 		stack.add(s);
 
-		//perform Depth-First Search
+		// perform Depth-First Search
 		while (!stack.empty()) {
 			Set<NFAState> states = stack.pop().getTo('e');
 			for (NFAState state : states) {
